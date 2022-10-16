8a411f1e8f0701958725386dde9d169eeb888e8c
fajarot <fajarrahmadyanto@gmail.com>
2022-10-16 16:11:48 +0700

    new template

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
 
+import schemas
+import crud
+
 app = FastAPI()
 
 
 @app.get('/')
 async def root():
-  return {"message": "Hello World"}
+    return {"message": "Hello World"}
 
 
 if __name__ == "__main__":
-  uvicorn.run(app, host="0.0.0.0", port="8080")
+    uvicorn.run(app, host="0.0.0.0", port="8080")
